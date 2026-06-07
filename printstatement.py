5e9e48e92cf31bded163101ae9662c126f170bce
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-06-07 10:03:40 +0400

    commit2

---
+++ printstatement.py
@@ -0,0 +1,11 @@
+#print a statement
+print("Welcome to the world of programming")
+# print a nunber
+print(4)
+#Print text on a new line
+print("hello python\n")
+#Print multiple things together
+print("hello",5)
+#Use the end argument
+print("welecome to",end="*")
+
