5b67d909d812be29313517e53d9f8b26f81c9cfc
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-06-13 10:18:48 +0400

    commit5

---
+++ concatnation.py
@@ -0,0 +1,13 @@
+str1='Hello'
+str2='world!'
+
+# using +
+print('str1+str2 =', str1 + str2)
+str ="string"
+
+print(str[3:])
+
+print(str[0:1])
+print(str[-1])
+message ="python is fun"
+print(message.upper())
---
+++ input.py
@@ -0,0 +1,3 @@
+print("Enter your name: ")
+name = input("enter your nane")
+print("\nhello",name, "\nwelcome of codingal")
