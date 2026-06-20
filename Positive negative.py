ea20f29bd2af95343d74867c2de0752f50885c7e
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-06-20 10:17:40 +0400

    commit7

---
+++ Positive negative.py
@@ -0,0 +1,10 @@
+num=int( input("enter a number "))
+if num> 0:
+    print(num,"is a positive number")
+else:
+    print (num,"is a negative number or zero")
+    
+     
+
+
+
---
+++ profit los.py
@@ -0,0 +1,8 @@
+cost_price=int(input("enter cost price "))
+sell_price=int(input("enter sell price "))
+if cost_price >sell_price :
+    print("loss of",cost_price-sell_price)
+else:
+    print("profit of",sell_price-cost_price)
+    
+    
