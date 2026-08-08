32cf5fba52c873bfaca0a7f34e02e0ea14d5c51e
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-08-08 10:22:14 +0400

    commit11

---
+++ loop.py
@@ -0,0 +1,17 @@
+n ="hello"
+for i in n:
+    print(i)
+    # for loop syntax
+for i in range(10):
+        print(i)
+for i in range (0,10,1):
+    print(i)
+print("even number ")
+for i in range (2,21,2):
+     print(i)
+print( "odd number")
+for i in range(1,20,2):
+     print("i")
+print(" reverse number")
+for i in range(20,0,-1):
+     print(i)
