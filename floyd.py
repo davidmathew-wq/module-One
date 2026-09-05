8614a91ca620fe555562f00dd04b4d3ac53d5e76
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-09-05 10:20:21 +0400

    commit18

---
+++ floyd.py
@@ -0,0 +1,9 @@
+print(" floyd traingle")
+n = int(input("enter number"))
+number = 1
+for i in range (0, n - 1):
+    for i in range(0, i ):
+
+        print(number, end=" " )
+        number = number + 1
+    print()
---
+++ halfpyramid.py
@@ -0,0 +1,7 @@
+print(" Half-pyramid of stars ")
+n = int(input("enter number"))
+for i in range (0, n - 1):
+    for i in range(0, i ):
+        print("*", end="" )
+    print()
+
