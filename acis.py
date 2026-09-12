e6f5de66156b629d5c04bed29a56140799eeb09b
davidmathew-wq <david.matthew3@globalschoolsummit.org>
2026-09-12 10:18:46 +0400

    commit18

---
+++ acis.py
@@ -0,0 +1,24 @@
+
+
+total = 0
+i = 1
+while i <= 5:
+    total = i + 2
+    i = i + 1
+print("Sum:", total)
+
+
+
+ 
+
+
+
+
+    
+
+ 
+
+
+
+
+
---
+++ numberguessing.py
@@ -0,0 +1,7 @@
+print(" NumberGuessing game")
+
+secret = 43
+attempts = 5
+while attempts >0:
+    attempts = attempts - 1
+    int(input("enter number from 1 to 50"))
